b728b825ac9b034c867b326cb631279ebcd4f909
Anjali <anjalik.2906@gmail.com>
2026-03-05 11:52:02 +0530

    commit

--- tut5.cpp
+++ tut5.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a,b;
+
+    cout<<"enter the value of a :\n"; /*<< this is called insertion operator*/
+    cin>>a;/*>> called extraction operator*/
+    cout<<"enter the value of b :\n"; /*<< this is called insertion operator*/
+    cin>>b;
+    cout<<"sum of a and b is "<<a+b<<endl;
     
+    return 0;
 }
