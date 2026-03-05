1b2361368544b7d78859efdef5dc47a466f650ad
Anjali <anjalik.2906@gmail.com>
2026-03-05 15:28:04 +0530

    commit

--- tut6.cpp
+++ tut6.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+// header files are of two types 
+// 1 sysytem header file: comes with the system.
+// 2. user defined : wriiten by programmer
 using namespace std;
 int main(){
-    
+    int a,b;
+    cin>>a>>b;
+cout<<"operators in c++"<<endl;
+cout<<"following are the operators"<<endl;
+cout<<"the addition "<<a+b<<endl;
+cout<<"the subtraction "<<a-b<<endl;
+
+cout<<"the multiplication "<<a*b<<endl;
+
+cout<<"the division "<<a/b<<endl;
+
+cout<<"the modo division "<<a%b<<endl;
+
+
 
 
 
