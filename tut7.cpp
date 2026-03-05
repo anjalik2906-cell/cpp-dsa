c44379de629acfd0f694d9e5316c4f0d990998d0
Anjali <anjalik.2906@gmail.com>
2026-03-05 23:32:23 +0530

    commit

--- tut7.cpp
+++ tut7.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+int c=48;
 int main(){
-    
+    int a,b,c;
+    cout<<"enter a "<<endl;
+    cin>>a;
+    cout<<"enter b "<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"sum is "<<c<<endl;
+    cout<<"global var c is "<<::c<<endl;
+
+    float d = 34.4f;
+    //f used here to specify that itis float ow by default 
+    // comlpier read it as a double val
+    long double e = 34.4l;
+    cout<<"val of d "<<d<<" val of e is "<<e;
+
     return 0;
 }
