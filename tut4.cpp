712178fc7a130da8dac78d415eb74fa59543f407
Anjali <anjalik.2906@gmail.com>
2026-03-05 11:12:22 +0530

    tut4

--- tut4.cpp
+++ tut4.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
+int glo = 6; //global variable
+void sum(){
+    int a;
+    cout<<glo;
+}
 /*dat types
 1) built in (int,float, char,boolean,double)
 2)derived(array, function, pointeer)
@@ -9,8 +14,15 @@ int main()
     int a=13 , b=17;
     float pi=3.14;
     char c= 'd';
-    cout<<"here value of a is"<<a<<"\nvalue of b is"<<b<<endl;
-    cout<<"\n value of pi is"<<pi<<endl;
-    cout<<"\n value of c is"<<c<<endl;
+    // cout<<"here value of a is"<<a<<"\nvalue of b is"<<b<<endl;
+    // cout<<"\n value of pi is"<<pi<<endl;
+    // cout<<"\n value of c is"<<c<<endl;
+    bool is_true =true;
+    //true 1 false 0
+    int glo =5; //local variable
+    glo =63; //val gets updated of local var
+    sum();
+    cout<<glo<<is_true;
     return 0;
 }
+//var can have max 255 char
