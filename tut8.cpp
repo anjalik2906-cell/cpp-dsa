0b75a5f62c6128109b95bfd43af6ca5abc66e38c
Anjali <anjalik.2906@gmail.com>
2026-03-06 12:29:28 +0530

    commit

--- tut8.cpp
+++ tut8.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
+    //constants in c++
+    const float a= 8.93;
+    cout<<a<<endl; //endl is a manipulator
+    int v=56,b=83,c=4;
+    cout<<"val of v "<<setw(4)<<v<<endl;
+    cout<<"val of b "<<setw(4)<<b<<endl;
+    cout<<"val of c "<<setw(4)<<c<<endl;
+
+    cout<<"val of v without setw "<<v<<endl;
+    cout<<"val of b without setw "<<b<<endl;
+    cout<<"val of c without setw "<<c<<endl;
+    
+    
     return 0;
 }
