d16d3e36887715333998f0cf37f238167cd6f80e
Anjali <anjalik.2906@gmail.com>
2026-04-19 18:38:57 +0530

    commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+int n;
+    // cin>>n;
+    // for(int i=0 ; i<n ; i++){
+    //     cout<<i<<endl;
+        
+    // }
+    // int x;
+    // cin>>x;
+    
+    // while(x>0){
+    //     x--;
+    //     cout<<x<<endl;
+        
+    // }
--- tut10.cpp
+++ tut10.cpp
@@ -11,20 +11,46 @@ int main (){
     // while(condition):{
     //     statements;
     // }
-    int n;
-    cin>>n;
-    for(int i=0 ; i<n ; i++){
-        cout<<i<<endl;
+    // int n;
+    // cin>>n;
+    // for(int i=0 ; i<n ; i++){
+    //     cout<<i<<endl;
         
-    }
-    int x;
-    cin>>x;
+    // }
+    // int x;
+    // cin>>x;
     
-    while(x>0){
-        x--;
-        cout<<x<<endl;
+    // while(x>0){
+    //     x--;
+    //     cout<<x<<endl;
         
-    }
+    // }
+    //example of infinite while loop
+    // int i=1;
+    // while(true){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+    //do whie loop--->syntax
+     /* do
+    {
+        statements;
+     }while(condition);
+        */
+    //    int i=1;
+    //    do    //loop runs atleast one time
+    //    {
+    //     cout<<i<<endl;
+    //     i++;
+    //    } while (i<=40);
+       int n;
+       cin>>n;
+       for(int i=1; i<=10; i++){
+        int c=n*i;
+        cout<<c<<endl;
+       }
+       
 
     return 0;
 }
