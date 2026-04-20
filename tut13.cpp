2876016b84af0c680e3237719f03e81fe49ed407
Anjali <anjalik.2906@gmail.com>
2026-04-20 23:17:26 +0530

    commit

---
+++ tut13.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[4]={23,87,54,74};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+
+
+    return 0;
+}
