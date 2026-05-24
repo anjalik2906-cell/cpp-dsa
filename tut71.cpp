a4144f88032a4fbb20631f73926298179b784aca
Anjali <anjalik.2906@gmail.com>
2026-05-24 19:45:41 +0530

    commit

---
+++ tut71.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+
+    int size, element;
+    cout<<"enter the size of vector"<<endl;
+    cin>>size;cout<<"enter the element of vector"<<endl;
+    for(int i=0; i<size; i++){
+        cin>>element;
+        v.push_back(element);
+    }
+    cout<<"the element of vector are "<<endl;
+    for(int i=0; i<size; i++){
+        cout<<v[i]<<endl;
+    }
+    v.pop_back();
+    cout<<"the element of vector are "<<endl;
+    
+    return 0;
+}
