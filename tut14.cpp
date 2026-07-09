5d3f93b43bfacb7f03cfe7b387a57f016e76a8cf
Anjali <anjalik.2906@gmail.com>
2026-07-09 21:45:14 +0530

    commit

---
+++ tut14.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> // this header file includes all standard libraries 
+using namespace std;
+int main(){
+    //data type
+    //int 
+    int x=10;
+    long long y=10000000000;
+    long double z=3.14159265358979323846;
+    // string s;
+    // cin>>s;
+    // cout<<x<<" "<<y<<" "<<z<<" "<<s<<endl;
+    //getline
+    string str;
+    getline(cin, str);  // to take input with spaces
+    
+    cout<<str<<endl;
+    return 0;
+    
+}
