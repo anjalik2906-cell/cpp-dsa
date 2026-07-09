01f16c019e3ccdd9a6e6e6270d58155e5fc105aa
Anjali <anjalik.2906@gmail.com>
2026-07-09 21:51:37 +0530

    commit

---
+++ tut15.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x>=18){
+        cout<<"adult"<<"/n";
+    }
+    else{
+        cout<<"nigga"<<endl;
+    }
+    return 0;
+}
