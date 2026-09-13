2aff59ef38bd8154ad9355ccd84fd09bf6b59cf8
Anjali <anjalik.2906@gmail.com>
2026-09-13 19:27:52 +0530

    modified:   tut15.cpp 	modified:   tut15.exe

--- tut15.cpp
+++ tut15.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x;
-    cin>>x;
-    if(x>=18){
-        cout<<"adult"<<"/n";
+    int m;
+    cin>>m;
+    if(m>=80 && m<=100){
+        cout<<"A";
     }
-    else{
-        cout<<"nigga"<<endl;
+     else if(m>=60 && m<=79){
+        cout<<"B";
+    }
+    else if(m>=50 && m<=59){
+        cout<<"C";
+    }
+    else if(m>=45 && m<=49){
+        cout<<"D";
+    }
+     else if(m>=25 && m<=44){
+        cout<<"E";
+    }
+     else{
+        cout<<"Fail!!!!!!!!";
     }
     return 0;
 }
